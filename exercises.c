0dbada7c02a65fdf8bb7151827f2c2b916876a0a
Your Name <you@example.com>
2025-03-20 20:56:23 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -86,8 +86,9 @@ Asume que popCurrent luego de eliminar un elemento se
 posiciona en el elemento anterior.
 */
 
-void eliminaElementos(List*L, int elem){
-
+void eliminaElementos(List*L, int elem)
+{
+   
 }
 
 /*
